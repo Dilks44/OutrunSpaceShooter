d6ddd5fd63795a581a5b4c0fabfeaaedd7bd9d6f
Dilks44 <Dilks44@gmail.com>
2019-04-07 17:34:48 -0400

    Fixes rockSpawning bug
    successfully builds to oculusGo and displays FPS while in game

--- Assets/myScripts/GameController.cs
+++ Assets/myScripts/GameController.cs
@@ -6,14 +6,12 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
 public class GameController : MonoBehaviour {
-
-
 	public GameObject ResetButton;
 	public GameObject BackToMainBtn;
-
 	public GameObject[] hazards;
 	public GameObject player;
 	public float ObjOntopPlayerBuffer;
+	public float ObjOntopEnemyBuffer;
 	public int hazardCount;
 	public int maxHazardsOnScreen;
 	private int numHazardsOnScreen;
@@ -23,10 +21,7 @@ public class GameController : MonoBehaviour {
 	public float startWait;
 	public float spawnWait;
 	public float dificultyMult;
-
 	public Text scoreText;
-
-
 	public int score;
 	private int highScore;
 
@@ -42,10 +37,8 @@ public class GameController : MonoBehaviour {
 		StartCoroutine (SpawnWaves());
 	}
 
-
 	void Update () {
 		if (player == null) {
-
 			if (score > highScore) {
 				Save ();
 				// Debug.Log ("Save called in Updade");
@@ -57,14 +50,12 @@ public class GameController : MonoBehaviour {
 		}
 	}
 
-
 	IEnumerator SpawnWaves() {
 		while (player != null) {
 			// Debug.Log ("startWait: " + startWait);
 			yield return new WaitForSeconds (startWait);
 			
 			for (int i = 0; i < hazardCount; i++) {
-
 				// make sure I dont spawn too many hazards and cause fps issues
 				if (numHazardsOnScreen < maxHazardsOnScreen) {
 
@@ -73,12 +64,19 @@ public class GameController : MonoBehaviour {
 						UnityEngine.Random.Range(xRange.x, xRange.y),
 						UnityEngine.Random.Range(yRange.x, yRange.y),
 						zSpawn);
-					while (isOntopPlayer (spawnPosition) == true) {
+
+					GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+					while (isOntopPlayer (spawnPosition) || isOntopOfAnyEnemy (spawnPosition, enemies) ) {
 						spawnPosition = new Vector3 (
 							UnityEngine.Random.Range(xRange.x, xRange.y),
 							UnityEngine.Random.Range(yRange.x, yRange.y),
 							zSpawn);
 					}
+
+					if (isOntopOfAnyEnemy(spawnPosition, enemies)) {
+						Debug.Log("wouldnt have spawned here due to enemeies");
+					}
+					
 					Quaternion spawnRotation = Quaternion.identity;
 					numHazardsOnScreen++;
 					Instantiate (hazard, spawnPosition, spawnRotation);
@@ -88,15 +86,22 @@ public class GameController : MonoBehaviour {
 						+", CUR = " +numHazardsOnScreen);
 				}
 
-
-				if (i == 0) {
-					// Debug.Log ("spawnWait: " + spawnWait);
-				}
 				yield return new WaitForSeconds (spawnWait);
 			}
+
 			startWait *= dificultyMult;
 			spawnWait *= dificultyMult;
 		}
+	}
+
+	private Boolean isOntopOfAnyEnemy(Vector3 location, GameObject[] enemies) {
+		Boolean isOnTop = false;
+		for (int j = 0; j < enemies.Length; j++) {
+			if (isOntopofEnemy (location, enemies[j])) {
+				isOnTop = true;
+			}
+		}
+		return isOnTop;
 	}
 
 	private Boolean isOntopPlayer(Vector3 location) {
@@ -105,13 +110,28 @@ public class GameController : MonoBehaviour {
 
 			if (Math.Abs (location.x - playerPos.x) < ObjOntopPlayerBuffer
 			    && Math.Abs (location.y - playerPos.y) < ObjOntopPlayerBuffer) {
-
-				//			Debug.Log ("Didnt spawn Object");
+					Debug.Log ("Rejected location because its on player");
 				//			Debug.Log ("Object spawning at x: " + location.x + ", y: " + location.y);
 				//			Debug.Log ("Player at x: " + playerPos.x + ", y: " + playerPos.y);
 				return true;
 			} else {
-				//			Debug.Log ("Not ontop of player");
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
+	private Boolean isOntopofEnemy(Vector3 location, GameObject enemy) {
+		if (enemy != null) {
+			Vector3 enemyPos = enemy.transform.position;
+			if (Math.Abs (location.x - enemyPos.x) < ObjOntopEnemyBuffer
+			    && Math.Abs (location.y - enemyPos.y) < ObjOntopEnemyBuffer) {
+					Debug.Log ("Rejected location because its on enemy");
+				//			Debug.Log ("Object spawning at x: " + location.x + ", y: " + location.y);
+				//			Debug.Log ("Player at x: " + enemyPos.x + ", y: " + enemyPos.y);
+				return true;
+			} else {
 				return false;
 			}
 		} else {
